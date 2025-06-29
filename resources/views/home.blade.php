84beb97579ce84b12382f9239e62148649219078
marckosalks <m4rcos.sales@gmail.com>
2025-06-29 15:34:21 -0300

    feat: arrumando ambiente

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Consulta de remessa</title>
+    <!-- icones Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Styles css -->
-    <link rel="stylesheet" href="/css/app.css"> <!-- seu CSS vem por último -->
-
-
-
+    <@vite('resources/css/app.css') 
 
 </head>
 
 <body>
-    <button type="button" class="btn btn-primary">Primary</button>
+    <div class="alinhar-busca">
+        <button type="submit" class="btn btn-secondary">
+            <i class="bi bi-search"></i>
+        </button>
+    </div>
 
     <div class="ajuste-tab">
 
